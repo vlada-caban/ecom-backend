1c885a8d17e2baef102b5dd82f27d76b3bf9f895
vlada_caban <vladaonly@gmail.com>
2023-06-30 11:47:42 -0400

    added models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  //when we delete Category, then category id is set to NULL under the product
+  onDelete: "SET NULL",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "product_has_tags",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "tag_has_products",
+});
 
 module.exports = {
   Product,
